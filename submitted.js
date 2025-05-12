dda6c9837f25ae0bdaefb3bb4f8302f03ae29b9f
Abdulrahman Al-Nachar <alnacharcs99@gmail.com>
2025-05-12 13:55:09 +0200

    Update submitted.js

--- submitted.js
+++ submitted.js
@@ -1,4 +1,4 @@
-// this is for my third objective stated above
+// this is for my third objective stated in the script.js
 let selectionStatement = document.getElementById("submitted-rating");
 let storedRating = localStorage.getItem("userRating");
 selectionStatement.innerHTML = "You selected " + storedRating + " out of 5";
